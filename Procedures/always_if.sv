43392ec2218414e0d7077985c8ebd3f393ed131f
Ken Zhang <ken@morsemicro.com>
2019-05-16 09:11:28 +1000

    Checked in always_if, creating mux via assign and always @(*).

---
+++ Procedures/always_if.sv
@@ -0,0 +1,22 @@
+module always_if 
+(
+  input a,
+  input b,
+  input sel_b1,
+  input sel_b2,
+  output wire out_assign,
+  output reg out_always
+);
+
+  assign out_assign = (sel_b1 && sel_b2) ? b : a;
+
+  always @(*) begin 
+    if (sel_b1 && sel_b2) begin
+      out_always = b;
+    end
+    else begin
+      out_always = a;
+    end
+  end
+
+endmodule
