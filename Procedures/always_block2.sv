d27a23e9244fffcb17e6ebb9ba9dfa7398fd21bb
Ken Zhang <ken@morsemicro.com>
2019-05-16 09:06:04 +1000

    Checked in always_block2. Introduced always @(posedge clk) for comb + flop.

---
+++ Procedures/always_block2.sv
@@ -0,0 +1,21 @@
+module always_block2 
+(
+  input clk,
+  input a,
+  input b,
+  output wire out_assign,
+  output reg out_always_comb,
+  output reg out_always_ff
+);
+
+  assign out_assign = a ^ b;
+
+  always @(*) begin
+    out_always_comb = a ^ b;
+  end
+
+  always @(posedge clk) begin
+    out_always_ff <= a ^ b;
+  end
+
+endmodule
