fdf99f6f29647ddf1222ebb60194557af400884f
Ken Zhang <ken@morsemicro.com>
2019-05-17 09:52:41 +1000

    Checked in always_nolatches. Use init before case to generalize all the default values which are not specified in the case statement.

---
+++ Procedures/always_nolatches.sv
@@ -0,0 +1,20 @@
+module always_nolatches 
+(
+  input [15:0] scancode,
+  output reg left,
+  output reg down,
+  output reg right,
+  output reg up 
+);
+
+  always @(*) begin
+    up = 1'b0; down = 1'b0; left = 1'b0; right = 1'b0;
+    case (scancode)
+      16'he06b : left = 1;
+      16'he072 : down = 1;
+      16'he074 : right = 1;
+      16'he075 : up = 1;
+    endcase
+  end
+
+endmodule
