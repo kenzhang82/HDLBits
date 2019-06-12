3953da199094ae75d76b0a62e5bea72967539613
Ken Zhang <ken@morsemicro.com>
2019-06-12 10:48:01 +1000

    Checked in thermostat.sv.

---
+++ Circuits/Combinational/thermostat.sv
@@ -0,0 +1,16 @@
+module thermostat 
+(
+  input too_cold,
+  input too_hot,
+  input mode,
+  input fan_on,
+  output heater,
+  output aircon,
+  output fan
+);
+
+  assign heater = mode & too_cold;
+  assign aircon = (~mode) & too_hot;
+  assign fan = heater | aircon | fan_on;
+
+endmodule
