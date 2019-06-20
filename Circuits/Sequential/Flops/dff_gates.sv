49002ad90e49c586b1a6975486b6336f1b3e36c3
Ken Zhang <ken@morsemicro.com>
2019-06-20 15:51:14 +1000

    Checked in dff_gates.sv, gates before flops.

---
+++ Circuits/Sequential/Flops/dff_gates.sv
@@ -0,0 +1,19 @@
+module dff_gates 
+(
+  input clk,
+  input x,
+  output z 
+);
+
+  reg q1, q2, q3;
+
+  always @(posedge clk) begin
+    q1 <= x ^ q1;
+    q2 <= x & (~q2);
+    q3 <= x | (~q3);
+  end
+
+  assign z = ~(q1 | q2 | q3);
+
+
+endmodule
