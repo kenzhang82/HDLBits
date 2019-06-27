75418c538d5784c228c6681bf5e54888fb5e825a
Ken <ken.zhang028@gmail.com>
2019-06-28 00:18:26 +1000

    Need to figure out how to switch AM and PM.

--- Circuits/Sequential/Counters/count_clock.sv
+++ Circuits/Sequential/Counters/count_clock.sv
@@ -1,4 +1,4 @@
-module count_clock 
+module count_clock
 (
   input clk,
   input reset,
@@ -6,7 +6,7 @@ module count_clock
   output pm,
   output [7:0] hh,
   output [7:0] mm,
-  output [7:0] ss 
+  output [7:0] ss
 );
 
   // Seconds
@@ -63,10 +63,26 @@ module count_clock
       pm <= 0;
     end
     else begin
-      if (ena && (ss[7:4]==4'd5) && (ss[3:0]==4'd9) && 
+      if (ena && (ss[7:4]==4'd5) && (ss[3:0]==4'd9) &&
                  (mm[7:4]==4'd5) && (mm[3:0]==4'd9)) begin
-        hh[3:0] <= 1;
-        hh[7:4] <= 0;
+      	if (hh < 8'h12) begin
+      		if (hh[3:0] < 4'd9) begin
+      			hh[3:0] <= hh[3:0] + 1'b1;
+      		end
+      		else begin
+      			hh[3:0] <= 0;
+      			if (hh[7:4] < 4'd1) begin
+      				hh[7:4] <= hh[7:4] + 1'b1;
+      			end
+      			else begin
+      				hh[7:4] <= 0;
+      			end
+      		end
+      	end
+      	else begin
+      		hh[3:0] <= 1;
+      		hh[7:4] <= 0;
+      	end
       end
     end
   end
