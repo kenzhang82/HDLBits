c39e7070fce6ee61ee84e263333e1953518ab786
Ken Zhang <ken@morsemicro.com>
2019-06-28 11:57:00 +1000

    Checked in shift4.sv, 4-bit shift register.

--- Circuits/Sequential/ShiftRegisters/shift4.sv
+++ Circuits/Sequential/ShiftRegisters/shift4.sv
@@ -8,6 +8,20 @@ module shift4
   output reg [3:0] q 
 );
   
-  // TODO
+  always @(posedge clk or posedge areset) begin
+    if (areset) begin
+      q <= 0;
+    end
+    else begin
+      if (load) begin
+        q <=  data;
+      end
+      else begin
+        if (ena) begin
+          q <= {1'b0, q[3:1]};
+        end
+      end
+    end
+  end
 
 endmodule
