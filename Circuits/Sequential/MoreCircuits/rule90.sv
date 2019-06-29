1ea1b8163d4ab2845aac9b37dba4fcb858e84d78
Ken <ken.zhang028@gmail.com>
2019-06-29 22:51:09 +1000

    Simplified rule90.sv.

--- Circuits/Sequential/MoreCircuits/rule90.sv
+++ Circuits/Sequential/MoreCircuits/rule90.sv
@@ -6,25 +6,13 @@ module rule90
 	output [511:0] q
 );
 
-	generate
-		always @(posedge clk) begin
-			if (load) begin
-				q <= data;
-			end
-			else begin
-				for (integer i = 0; i < 512; i = i + 1) begin
-					if (i == 0) begin
-						q[i] <= 1'b0 ^ q[i + 1];
-					end
-					else if (i == 511) begin
-						q[i] <= q[i - 1] ^ 1'b0;
-					end
-					else begin
-						q[i] <= q[i - 1] ^ q[i + 1];
-					end
-				end
-			end
+	always @(posedge clk) begin
+		if (load) begin
+			q <= data;
 		end
-	endgenerate
+		else begin
+			q <= { 1'b0, q[511:1] } ^ { q[510:0], 1'b0 };
+		end
+	end
 
 endmodule
