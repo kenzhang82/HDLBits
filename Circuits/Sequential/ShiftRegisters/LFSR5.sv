1ae0b55bff16b7f60e2945ac94bd03913140177d
Ken <ken.zhang028@gmail.com>
2019-06-29 12:46:39 +1000

    Checked in LFSR5.sv, verilog from circuit diagram.

--- Circuits/Sequential/ShiftRegisters/LFSR5.sv
+++ Circuits/Sequential/ShiftRegisters/LFSR5.sv
@@ -1,15 +1,20 @@
-module LFSR5 
+module LFSR5
 (
   input clk,
   input reset,
   output [4:0] q
 );
-  
+
   always @(posedge clk) begin
     if (reset) begin
       q <= 1;
     end
     else begin
+    	q[4] <= 1'b0 ^ q[0];
+    	q[3] <= q[4];
+    	q[2] <= q[3] ^ q[0];
+    	q[1] <= q[2];
+    	q[0] <= q[1];
     end
   end
 
