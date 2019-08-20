99bbc2e5571906f1aad43acff98a34f937d6435b
Ken Zhang <ken@morsemicro.com>
2019-08-20 17:59:54 +1000

    Simplify state transition logic for lemmings2.sv.

--- Circuits/Sequential/FSM/lemmings2.sv
+++ Circuits/Sequential/FSM/lemmings2.sv
@@ -10,7 +10,10 @@ module lemmings2
 	output aaah
 );
 
-	parameter LEFT = 2'd0, LEFT_GROUND = 2'd1, RIGHT = 2'd2, RIGHT_GROUND = 2'd3;
+	parameter LEFT = 2'd0,
+            LEFT_GROUND = 2'd1,
+            RIGHT = 2'd2,
+            RIGHT_GROUND = 2'd3;
 
 	reg [1:0] curr_dir, next_dir;
 
@@ -26,38 +29,10 @@ module lemmings2
 
 	always @(*) begin
 		case (curr_dir)
-			LEFT: begin
-				if (ground) begin
-					next_dir = bump_left ? RIGHT : LEFT;
-				end
-				else begin
-					next_dir = LEFT_GROUND;
-				end
-			end
-			RIGHT: begin
-				if (ground) begin
-					next_dir = bump_right ? LEFT : RIGHT;
-				end
-				else begin
-					next_dir = RIGHT_GROUND;
-				end
-			end
-			LEFT_GROUND: begin
-				if (ground) begin
-					next_dir = LEFT;
-				end
-				else begin
-					next_dir = LEFT_GROUND;
-				end
-			end
-			RIGHT_GROUND: begin
-				if (ground) begin
-					next_dir = RIGHT;
-				end
-				else begin
-					next_dir = RIGHT_GROUND;
-				end
-			end
+			LEFT:  next_dir = ground ? (bump_left ? RIGHT : LEFT) : LEFT_GROUND;
+			RIGHT: next_dir = ground ? (bump_right ? LEFT : RIGHT) : RIGHT_GROUND;
+			LEFT_GROUND:  next_dir = ground ? LEFT : LEFT_GROUND;
+			RIGHT_GROUND: next_dir = ground ? RIGHT : RIGHT_GROUND;
 		endcase
 	end
 
